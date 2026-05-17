a081116712febaf00b505ef7f8fbe297732e72b0
binyamin <binyaminahmed594@gmail.com>
2026-05-17 19:58:33 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
